88896b0c0a3fdc75ba36d3d7fbad706fe8ebb09a
luminous-whispers <2291303762@qq.com>
2024-04-14 18:36:46 +0800

    fix bug in testcase_generator

--- fuzz.sh
+++ fuzz.sh
@@ -1,31 +1,72 @@
 #!/bin/bash
+export NUM_SEED=30
+export HEARTBEAT="/tmp/fuzz_heartbeat"
+export ERROR="/tmp/fuzz_error"
+# seed path: seeds
 
-DISPATCHER="dispatcher.sh"
-GENERATOR="testcase_generator.sh"
-MONITOR="monitor.sh"
+# variables
+if [[ ! -p $ERROR ]]; then
+  mkfifo $ERROR
+fi
+declare -A pids
+round=0
+touch $HEARTBEAT
 
-bash "$MONITOR" &
-MONITOR_PID=$!
-bash "$DISPATCHER" &
-DISPATCHER_PID=$!
-bash "$GENERATOR" & && GENERATOR_PID=$!
-
-function cleanup{
-  kill $MONITOR_PID
-  kill $DISPATCHER_PID
-  kill $GENERATOR_PID
-  wait $MONITOR_PID
-  wait $DISPATCHER_PID
-  wait $GENERATOR_PID
-  echo "fuzz exited"
-  return 0
+function cleanup {
+  echo "[Fuzz] exit, please wait..."
+	timeout 5 redis-cli shutdown
+  kill -SIGINT ${pids[tcg]}
+  wait ${pids[tcg]}
+	rm $HEARTBEAT
+  rm $ERROR
+	exit 0
 }
-trap cleanup SIGINT
+trap cleanup SIGINT SIGTERM
 
-while ture; do
-  echo "fuzz round start, generating testcase"
-  touch "start_file" #?
-  echo "fuzzing in progress..."
-  echo "fuzz round ended."
+( # catch error fifo
+  while true; do
+    if read line < $ERROR; then
+      echo "[Fuzz][Error] $line"
+      kill -SIGINT $$
+      break
+    fi
+  done
+) &
+
+
+function guard {
+  local cmd="$1"
+  local -n pid_var=$2
+  $cmd &
+  pid_var=$!
+}
+
+function load_data {
+  redis-server --port 6379 &
+  while [ "$(redis-cli PING)" != "PONG" ]; do
+    sleep 1
+  done
+  echo "[Fuzz] redis server is ready"
+
+  for seed_file in seeds/*; do 
+    seed=$(<"$seed_file") 
+    if [ -z $(redis-cli ZScore pool "$seed") ]; then
+      # 如不存在, 新建
+      redis-cli ZAdd pool 3 "$seed"
+    fi
+  done
+  echo "[Fuzz] successfully load seeds"
+}
+
+
+load_data
+
+guard "./testcase_generator.sh" pids[tcg] 
+sleep 1 # wait for subroutines
+
+while true; do
+	((round++))
+  echo -e "\n[Fuzz] round: $round"
+	echo "$round" > $HEARTBEAT
+  sleep 5
 done
-
--- testcase_generator.sh
+++ testcase_generator.sh
@@ -1,30 +1,51 @@
-#!/bin/bash
-
-redis-server &
-REDIS_PID=$!
-if ! redis-cli ping; then
-  echo "启动Redis失败"
-  exit 1
-fi
-
-trap "kill $REDIS_PID" EXIT
-
-load_testcases() {
-  local testcase_dir="seeds"
-  local testcase_key="testcases"
-  for seed_
+#!/bin/bash 
+function mutator {
+  read in
+	echo "${in}_mutated"
 }
 
-
-mutator(){
-  local testcase=$(cat -)
-  echo "$testcase"
+function guard {
+  "$@" > /dev/null 2>&1
+  return $?
 }
 
-on_new_fuzz_round() {
-
+function cleanup {
+  touch $HEARTBEAT # end inotifywait
+  exit 0
 }
+trap cleanup SIGINT SIGTERM
 
-echo msg | mutate > "hello.txt"
+while guard inotifywait -e modify "$HEARTBEAT"; do 
+  case $? in
+    0) # noraml eixt
+      read round < $HEARTBEAT
 
+      seed="$(redis-cli --raw ZRevRange pool 0 0)"
+      # if pool is used up, exit 1
+      if [ -z "$seed" ]; then
+        echo "pool used up" > "$ERROR"
+        exit 1
+      fi
+      score=$(redis-cli --raw ZScore pool "$seed")
+      echo "$seed, $score"
+
+      # mutate seed
+      # new_seed=$(mutator "$seed")
+      guard redis-cli ZAdd pool $(($score-1)) "$seed"
+
+      # remove seed with score 0
+      guard redis-cli ZRemRangeByScore pool -inf 0 > /dev/null
+
+      # when szie > 200, remove seed with least score
+      while [ $(redis-cli ZCard pool) -gt 200 ]; do
+        guard redis-cli ZRemRangeByRank pool 0 0  
+      done
+      sleep 1
+      ;;
+    *) # timeout
+      echo "inotifywait error" > "$ERROR"
+      exit 1
+      ;;
+  esac
+done
 
